31da55d4bbe9a23848881a26aed7d876100b805f
Borys <B.Glushchenko21@gmail.com>
2021-10-08 22:05:18 +0300

    class TV

---
+++ TVSet/TV.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TVSet
+{
+    class TV
+    {
+        public string NextСhannel(string[] channels, ref int channel)
+        {
+            channel++;
+            return channels[channel];
+        }
+        public string BackСhannel(string[] channels, ref int channel)
+        {
+            channel--;
+            return channels[channel];
+        }
+        public string СurrentСhannel(string[] channels, ref int channel)
+        {
+            return channels[channel];
+        }
+    }
+}
