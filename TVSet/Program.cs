b3383b01b66f9d1fb08f9ba23bba8ec495b8bc02
Borys <B.Glushchenko21@gmail.com>
2021-10-08 23:21:54 +0300

    oop Program

--- TVSet/Program.cs
+++ TVSet/Program.cs
@@ -9,43 +9,18 @@ namespace TVSet
     class Program
     {
         static void Main(string[] args)
-        {           
-            Random random = new Random();
+        {
+            int channel = 5;
+
             TV tv = new TV();
+            tv.ChannelMeny();
 
-            string[] channels = new string[20];
+            tv.СurrentСhannel(channel);
+            
+            tv.NextСhannel();
 
-            for (int i = 0; i < channels.Length; i++)
-            {
-                int randomNumber = random.Next(1, 100);
-                
-                if(randomNumber % 2 == 0)
-                {
-                    channels[i] = "On";
-                }
-                else
-                {
-                    channels[i] = "Off";
-                }
-            }
-            for (int i = 0; i < channels.Length; i++)
-            {
-                
-                Console.WriteLine($"{i+1} {channels[i]}");
-            }
-            Console.WriteLine("");
+            tv.BackСhannel();
 
-            int randomChannel = random.Next(1, 20);
-            randomChannel--;
-
-            string currentСhannel = tv.СurrentСhannel(channels, ref randomChannel);
-            Console.WriteLine($"Канал {randomChannel + 1} {currentСhannel}");
-
-            string nextСhannel = tv.NextСhannel(channels, ref randomChannel);
-            Console.WriteLine($"Канал {randomChannel + 1} {nextСhannel}");
-
-            string backСhannel = tv.BackСhannel(channels, ref randomChannel);
-            Console.WriteLine($"Канал {randomChannel + 1} {backСhannel}");
             Console.ReadKey();
         }
     }    
