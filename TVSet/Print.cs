cba6a2dcc019d111fbc57bc4277a3eee13ecd1be
Borys <B.Glushchenko21@gmail.com>
2021-10-09 21:21:32 +0300

    oop Print

---
+++ TVSet/Print.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TVSet
+{
+    static class Print
+    {
+        static int channel = 0;
+        public static void NextСhannel(ChannelTV[] channelTV)
+        {
+            channel++;
+            Console.WriteLine($"Канал {channel} {channelTV[channel].onOff}");
+            Console.WriteLine();
+        }
+        public static void BackСhannel(ChannelTV[] channelTV)
+        {
+            channel--;
+            Console.WriteLine($"Канал {channel} {channelTV[channel].onOff}");
+            Console.WriteLine();
+        }
+        public static void СurrentСhannel(ChannelTV[] channelTV, int numberChannel)
+        {
+            channel = numberChannel;
+            Console.WriteLine($"Канал {channel} {channelTV[channel].onOff}");
+            Console.WriteLine();
+        }
+    }
+}
