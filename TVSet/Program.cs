1fe7f07eafcfebc57e87c77d4d6eaab89cb2de48
Borys <B.Glushchenko21@gmail.com>
2021-10-09 21:22:39 +0300

    new opp Program

--- TVSet/Program.cs
+++ TVSet/Program.cs
@@ -10,18 +10,66 @@ namespace TVSet
     {
         static void Main(string[] args)
         {
-            int channel = 5;
+            ChannelTV[] channelTV = new ChannelTV[100];
+            TV tv = new TV(channelTV);
+            tv.Channels();
 
-            TV tv = new TV();
-            tv.ChannelMeny();
-
-            tv.СurrentСhannel(channel);
-            
-            tv.NextСhannel();
-
-            tv.BackСhannel();
-
-            Console.ReadKey();
+            bool flagChannelStart = false;
+            while (flagChannelStart == false)
+            {
+                int channel;
+                Console.WriteLine("Введите номер канала от 0 до 99:");
+                Console.WriteLine("Для пролистывания каналов вперед, нажмите: w");
+                Console.WriteLine("Для пролистывания каналов назад, нажмите: s");
+                Console.WriteLine("Для выключение телевизора, нажмите: off");
+                Console.WriteLine();
+                string way = Console.ReadLine();
+                switch (way)
+                {
+                    case "w":
+                        try
+                        {
+                            Print.NextСhannel(channelTV);
+                        }
+                        catch
+                        {
+                            Print.СurrentСhannel(channelTV, 0);
+                        }
+                        break;
+                    case "s":
+                        try
+                        {
+                            Print.BackСhannel(channelTV);
+                        }
+                        catch
+                        {
+                            Print.СurrentСhannel(channelTV, 99);
+                        }
+                        break;
+                    case "off":
+                        flagChannelStart = true;
+                        break;
+                    default:
+                        try
+                        {
+                            channel = Int32.Parse(way);
+                        }
+                        catch
+                        {
+                            channel = -1;
+                        }
+                        if (channel >= 0 && channel < 100)
+                        {
+                            Print.СurrentСhannel(channelTV, channel);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Вы ввели не корректный номер канала");
+                            Console.WriteLine();
+                        }
+                        break;
+                }               
+            }
         }
     }    
 }
