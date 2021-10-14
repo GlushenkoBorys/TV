2d0f5fa016e01c55dc56a0fe1d4bb405f0331ffd
Borys <B.Glushchenko21@gmail.com>
2021-10-14 13:12:16 +0300

    TV

--- TVSet/TV.cs
+++ TVSet/TV.cs
@@ -6,37 +6,70 @@ using System.Threading.Tasks;
 
 namespace TVSet
 {
-    struct ChannelTV
-    {
-        internal string onOff;
-        internal int numberChannel;
-    }
     class TV 
-    {
-        Random random = new Random();
-        ChannelTV[] channelTV;
+    {        
+        int [] channelsTV;
+        int channel = 0;
+        int channelTV;
 
-        public TV(ChannelTV[] channelTV)
+        public TV(int n)
         {
-            this.channelTV = channelTV;
+            channelsTV = new int[n];
         }
         public void Channels()
         {
-            for (int i = 0; i < channelTV.Length; i++)
+            Random random = new Random();
+            for (int i = 0; i < channelsTV.Length; i++)
             {
                 int randomNumber = random.Next(1, 100);
-
-                if (randomNumber % 2 == 0)
-                {
-                    channelTV[i].onOff = "On";
-                }
-                else
-                {
-                    channelTV[i].onOff = "Off";
-                }
-
-                channelTV[i].numberChannel = i;
+                channelsTV[i] = randomNumber;               
             }
+        }
+
+        public void SetСhannel(string numberChannel)
+        {
+
+            int currentChannel = Int32.Parse(numberChannel);
+
+            if (currentChannel >= 0 && currentChannel < 100)
+            {
+                channel = currentChannel;
+                channelTV = channelsTV[channel];
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели некорректный номер канала");
+                Console.WriteLine();
+            }
+            Сhannel();
+        }
+
+        public void NextСhannel()
+        {
+            channel++;
+            if(channel == 100)
+            {
+                channel = 0;
+            }
+            channelTV = channelsTV[channel];         
+            Сhannel();
+        }
+
+        public void BackСhannel()
+        {
+            channel--;
+            if (channel == -1)
+            {
+                channel = 99;
+            }
+            channelTV = channelsTV[channel];
+            Сhannel();
+        }
+
+        public  void Сhannel()
+        {
+            Console.WriteLine($"Канал {channel} '{channelTV}'");
+            Console.WriteLine();
         }
     }
 }
